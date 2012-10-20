2bd02bdd320beafe3a3638421dda9ee28128faac
atcg <evanmelstad@ucla.edu>
2012-10-20 12:00:02 -0700

    ...

--- FGT2.pl
+++ FGT2.pl
@@ -2,6 +2,7 @@
 
 #edited on spaceship 8/7/2012
 #Edited in Ubuntu 10/19/2012!
+#Komodo
 
 #Based on Federico Abascal's "mitobank.pl" (http://darwin.uvigo.es/)
 #Copyright (C) 2012 Evan McCartney-Melstad, based on Federico Abascal's
